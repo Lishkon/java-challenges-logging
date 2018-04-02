ec8f8a9b00a8e8a4545b68068467475ea5ec0ac0
Lishkon <lishkon@gmail.com>
2018-04-02 12:17:08 +0300

    Trimming challange solved

--- src/main/java/kyu8/RemoveChars.java
+++ src/main/java/kyu8/RemoveChars.java
@@ -1,4 +1,7 @@
 package kyu8;
 
 public class RemoveChars {
+    public static String remove(String str) {
+        return str.substring(1, str.length()-1);
+    }
 }
--- src/test/java/kyu8/RemoveCharsTest.java
+++ src/test/java/kyu8/RemoveCharsTest.java
@@ -1,4 +1,15 @@
 package kyu8;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class RemoveCharsTest {
+    @Test
+    public void testRemoval() {
+        assertEquals("loquen", RemoveChars.remove("eloquent"));
+        assertEquals("ountr", RemoveChars.remove("country"));
+        assertEquals("erso", RemoveChars.remove("person"));
+        assertEquals("lac", RemoveChars.remove("place"));
+    }
 }
