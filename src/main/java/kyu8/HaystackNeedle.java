72b4b74c526ae6f4eb775e4616cb3b038bf0de09
Aleksey Lychak <mybox-aleks@inbox.ru>
2018-05-02 15:47:18 +0300

    Started a Needle search challenge

---
+++ src/main/java/kyu8/HaystackNeedle.java
@@ -0,0 +1,20 @@
+package kyu8;
+
+/**
+ *    Write a function findNeedle() that takes an array full of junk but containing one "needle"
+ *    After your function finds the needle it should return a message (as a string) that says:
+ *    "found the needle at position " plus the index it found the needle, so:
+ *
+ *    For instance, this code
+ *    <code>findNeedle(new Object[] {"hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"})<code>
+ *
+ *    should return:
+ *
+ *    <quote>"found the needle at position 5"</quote>
+ * */
+public class HaystackNeedle {
+    public static String findNeedle (Object[] haystack) {
+        String needle = "needle";
+        return needle;
+    }
+}
