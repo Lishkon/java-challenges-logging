c9cb13275399e1ba896aadd43c7636e3b5d1d6aa
Aleksey Lychak <mybox-aleks@inbox.ru>
2018-05-07 18:02:14 +0300

    Corrected a little bit

--- src/main/java/kyu8/ConvertToBinary.java
+++ src/main/java/kyu8/ConvertToBinary.java
@@ -1,4 +1,16 @@
 package kyu8;
-
+/**
+ * This class contains the <code>toBinary()</code> method that converts the regular number to a Binary one
+ * */
 public class ConvertToBinary {
+    /**
+     *  This method converts the regular number to <code>Binary</code> code
+     *
+     *  @param n which is the Integer type
+     *  @return the converted Binary integer
+     * */
+    public static int toBinary(int n) {
+        String parsed = Integer.toBinaryString(n);
+        return Integer.parseInt(parsed);
+    }
 }
