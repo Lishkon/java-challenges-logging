2096ec922c2dc00e85be23368b872627acf21c5e
lishkon <lishkon@gmail.com>
2018-10-21 02:11:32 +0300

    one more task solved

--- src/main/java/kyu8/StringRepeat.java
+++ src/main/java/kyu8/StringRepeat.java
@@ -1,4 +1,25 @@
 package kyu8;
 
 public class StringRepeat {
+    public static String repeatStr(final int repeat, final String string) {
+        String output = "";
+        for(int i = 0; i < repeat; i++) {
+            output += string;
+        }
+        return output;
+    }
 }
+
+//Better solution:
+//
+//public class Solution {
+//    public static String repeatStr(final int repeat, final String string) {
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < repeat; i++) {
+//            sb.append(string);
+//        }
+//
+//        return sb.toString();
+//    }
+//}
