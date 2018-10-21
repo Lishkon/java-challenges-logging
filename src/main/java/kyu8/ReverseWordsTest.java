ab57d7cc5ad0b38f041ab3c5a8dd3f95a49216c6
lishkon <lishkon@gmail.com>
2018-10-22 00:24:40 +0300

    New task completed: ReduceButGrow

---
+++ src/main/java/kyu8/ReverseWordsTest.java
@@ -0,0 +1,18 @@
+package kyu8;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ReverseWordsTest {
+    @Test
+    public void testSomething() {
+        assertEquals("eating like I", ReverseWords.reverseWords("I like eating"));
+        assertEquals("flying like I", ReverseWords.reverseWords("I like flying"));
+        assertEquals("nice is world The", ReverseWords.reverseWords("The world is nice"));
+    }
+}
+
+
+
+
+
