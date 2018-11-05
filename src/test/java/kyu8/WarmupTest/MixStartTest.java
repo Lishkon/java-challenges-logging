37818901cdb659e82cc0d985f1e6437b40ee40d4
lishkon <lishkon@gmail.com>
2018-11-06 01:33:51 +0200

    Another Warmup task is solved

---
+++ src/test/java/kyu8/WarmupTest/MixStartTest.java
@@ -0,0 +1,20 @@
+package kyu8.WarmupTest;
+
+import kyu8.Warmup.MixStart;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MixStartTest {
+    @Test
+    public void test() {
+        Assert.assertTrue(MixStart.mixStart("mix snacks"));
+        Assert.assertTrue(MixStart.mixStart("pix snacks"));
+        Assert.assertFalse(MixStart.mixStart("piz snacks"));
+        Assert.assertFalse(MixStart.mixStart("papaguinea"));
+        Assert.assertFalse(MixStart.mixStart("ixis rutus"));
+        Assert.assertFalse(MixStart.mixStart("ix"));
+        Assert.assertFalse(MixStart.mixStart("n"));
+        Assert.assertFalse(MixStart.mixStart(""));
+
+    }
+}
