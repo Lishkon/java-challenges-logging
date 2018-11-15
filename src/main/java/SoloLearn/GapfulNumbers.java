0701d5f6062857e6ece8577d643c697c47289a50
lishkon <lishkon@gmail.com>
2018-11-15 23:01:53 +0200

    Gapful Number challenge updated

--- src/main/java/SoloLearn/GapfulNumbers.java
+++ src/main/java/SoloLearn/GapfulNumbers.java
@@ -1,9 +1,5 @@
 package SoloLearn;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class GapfulNumbers {
     public static boolean gapNum(int num) {
 
