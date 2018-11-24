02c34ee30857e5ff374b765a1d4dd8adfb27588b
lishkon <lishkon@gmail.com>
2018-11-25 01:02:05 +0200

    One more FizzBuzz challenge solved

--- src/main/java/CodingBat/FizzBuzzCode/FizzArray3.java
+++ src/main/java/CodingBat/FizzBuzzCode/FizzArray3.java
@@ -1,4 +1,18 @@
 package CodingBat.FizzBuzzCode;
+/*
+ * url - https://codingbat.com/prob/p142539?parent=/doc/practice/fizzbuzz-code.html
+ **/
 
 public class FizzArray3 {
+    public int[] fizzArray3 (int start, int end) {
+        int size = end-start;
+        int[] output = new int[size];
+        for (int i = 0; i < size; ){
+            for (int j = start; j < end; j++) {
+                output[i] = j;
+                i++;
+            }
+        }
+        return output;
+    }
 }
--- src/test/java/CodingBat/FizzBuzzTest/FizzArray3Test.java
+++ src/test/java/CodingBat/FizzBuzzTest/FizzArray3Test.java
@@ -1,4 +1,25 @@
 package CodingBat.FizzBuzzTest;
 
+import CodingBat.FizzBuzzCode.FizzArray3;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FizzArray3Test {
+    @Test
+    public void twoItemsArrayTest() {
+        FizzArray3 fa = new FizzArray3();
+        Assert.assertArrayEquals(new int[]{1,2}, fa.fizzArray3(1,3));
+    }
+
+    @Test
+    public void fiveItemsArrayTest() {
+        FizzArray3 fa = new FizzArray3();
+        Assert.assertArrayEquals(new int[]{5, 6, 7, 8, 9}, fa.fizzArray3(5,10));
+    }
+
+    @Test
+    public void sevenItemsArrayTest() {
+        FizzArray3 fa = new FizzArray3();
+        Assert.assertArrayEquals(new int[]{11, 12, 13, 14, 15, 16, 17}, fa.fizzArray3(11,18));
+    }
 }
