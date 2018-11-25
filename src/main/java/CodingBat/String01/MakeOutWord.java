0620b70944d293a5756645b2c755c9d520d62a1f
lishkon <lishkon@gmail.com>
2018-11-25 02:01:17 +0200

    one more String task solved

---
+++ src/main/java/CodingBat/String01/MakeOutWord.java
@@ -0,0 +1,7 @@
+package CodingBat.String01;
+
+public class MakeOutWord {
+    public String makeOutWord(String out, String word) {
+        return out.substring(0,2)+word+out.substring(2);
+    }
+}
