7e609b897a82852f2d978cdf335e4f5df59493f0
lishkon <lishkon@gmail.com>
2018-11-26 17:22:44 +0200

    better code for HasBad

--- src/main/java/CodingBat/String01/HasBad.java
+++ src/main/java/CodingBat/String01/HasBad.java
@@ -2,18 +2,14 @@ package CodingBat.String01;
 
 public class HasBad {
     public boolean hasBad(String str) {
-        boolean res = false;
-        if (str.length() > 3) {
-            res = str.substring(0, 3).equals("bad");
+        if (str.length()>=3 && str.substring(0, 3).equals("bad")) {
+            return true;
         }
 
-        if (str.length() >= 4) {
-            res = str.substring(1, 4).equals("bad");
+        if (str.length()>=4 && str.substring(1, 4).equals("bad")) {
+            return true;
         }
 
-        if (str.startsWith("bad")) {
-            res = true;
-        }
-        return  res;
+        return false;
     }
 }
