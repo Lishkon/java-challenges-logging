b2859a6ba54ec7e068c766e25c3b1d89e2aa516d
lishkon <lishkon@gmail.com>
2018-11-29 02:28:20 +0200

    Keep working on PigLatin kata

--- src/main/java/Codewars/kyu5/SimplePigLatin.java
+++ src/main/java/Codewars/kyu5/SimplePigLatin.java
@@ -1,4 +1,50 @@
 package Codewars.kyu5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+ * Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+ * @url: https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/java
+ **/
+
 public class SimplePigLatin {
+    public String simplePigLatin(String str) {
+        String res = "";
+
+        String[] strArr = str.split(" ");
+
+//        for (int i = 0; i < strArr.length; i++) {
+//            strArr[i] = pigify(strArr[i]);
+//            res += strArr[i] + " ";
+//        }
+//        System.out.println(Arrays.toString(strArr));
+
+        // This solution seems to be working but it doesn't work fine if the string contains punctuations
+        ArrayList<String> list = new ArrayList<String>();
+        for (String s : strArr) {
+            list.add(pigify(s));
+        }
+        for (String s: list) {
+            res += s + " ";
+        }
+
+
+        return res.trim();
+    }
+
+    static String pigify(String str) {
+        String clear = str.replaceAll("\\W", " ").trim();
+        String res = clear + "ay";
+        if  (clear.length()==0) {
+            res = "";
+        }
+        if (clear.length()==2) {
+            res = String.valueOf(clear.charAt(1)) + String.valueOf(clear.charAt(0)) + "ay";
+        }
+        if (clear.length()>2) {
+            res = clear.substring(1) + String.valueOf(clear.charAt(0)) + "ay";
+        }
+        return res;
+    }
 }
--- src/test/java/Codewars/kyu5/SimplePigLatinTest.java
+++ src/test/java/Codewars/kyu5/SimplePigLatinTest.java
@@ -1,4 +1,54 @@
 package Codewars.kyu5;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SimplePigLatinTest {
+    @Test
+    public void testSomethingTest() {
+        SimplePigLatin spl = new SimplePigLatin();
+        Assert.assertEquals("Iay maay gtestinay sthiay gstrinay", spl.simplePigLatin("I am testing this string"));
+    }
+
+    @Test
+    public void helloWorldTest() {
+        SimplePigLatin spl = new SimplePigLatin();
+        Assert.assertEquals("elloHay orldway !", spl.simplePigLatin("Hello world !"));
+    }
+
+    @Test
+    public void pigLatinIsCoolTest() {
+        SimplePigLatin spl = new SimplePigLatin();
+        Assert.assertEquals("igPay atinlay siay oolcay", spl.simplePigLatin("Pig latin is cool"));
+    }
+
+    @Test
+    public void simplePigifyTest() {
+        Assert.assertEquals("lexaay", SimplePigLatin.pigify("alex"));
+    }
+
+    @Test
+    public void singleCharPigifyTest() {
+        Assert.assertEquals("Iay", SimplePigLatin.pigify("I"));
+    }
+
+    @Test
+    public void twoCharsPigifyTest() {
+        Assert.assertEquals("sIay", SimplePigLatin.pigify("Is"));
+    }
+
+    @Test
+    public void punctuatedTest() {
+        Assert.assertEquals("Iay", SimplePigLatin.pigify("I!"));
+    }
+
+    @Test
+    public void punctuated2Test() {
+        Assert.assertEquals("amIay", SimplePigLatin.pigify("Iam!"));
+    }
+
+    @Test
+    public void emptyTest() {
+        Assert.assertEquals("", SimplePigLatin.pigify(""));
+    }
 }
