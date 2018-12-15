2145bd2cdaf7aee13b33964246185c514f10e6f9
lishkon <lishkon@gmail.com>
2018-12-16 00:39:54 +0200

    Tutorials for Day15 are done

--- src/main/java/HackerRank/Day15Tutorial/LinkedList.java
+++ src/main/java/HackerRank/Day15Tutorial/LinkedList.java
@@ -1,4 +1,66 @@
 package HackerRank.Day15Tutorial;
 
 public class LinkedList {
+
+    // Properties
+    Node head;
+    int count;
+
+    // Constructors
+    public LinkedList() {
+        head = null;
+        count = 0;
+    }
+
+    public LinkedList(Node newHead) {
+        head = newHead;
+        count = 1;
+    }
+    // Methods
+
+    // add
+    public void add(int newData) {
+        Node temp = new Node(newData);
+        Node current = head;
+        while (current.getNext()!= null) {
+            current = current.getNext();
+        }
+        current.setNext(temp);
+        count++;
+    }
+    // get
+    public int get(int index) {
+        if (index<=0) {
+            return -1;
+        }
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return current.getData();
+    }
+    // size
+
+    public int size() {
+        return count;
+    }
+
+    // isEmpty
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    // remove
+    public void remove() {
+        Node current = head;
+        while (current.getNext().getNext() != null) {
+            current = current.getNext();
+        }
+        current.setNext(null);
+        count--;
+    }
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/HackerRank/Day15Tutorial/Node.java
+++ src/main/java/HackerRank/Day15Tutorial/Node.java
@@ -1,4 +1,34 @@
 package HackerRank.Day15Tutorial;
 
 public class Node {
+    // Properties
+    Node next;
+    int data;
+
+    public Node (int newData) {
+        data = newData;
+        next = null;
+    }
+
+    public Node (int newData, Node newNext) {
+        data = newData;
+        next = newNext;
+    }
+
+    // Getters and Setters
+    public int getData() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setData(int newData) {
+        data = newData;
+    }
+
+    public void setNext(Node newNext) {
+        next = newNext;
+    }
 }
